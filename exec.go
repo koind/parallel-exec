8cd61eafb7762446ec82221c0b8ff9f91bb69a11
Damir15 <krosava_15d@mail.ru>
2019-07-07 13:36:41 +0600

    Changed

--- exec.go
+++ exec.go
@@ -7,34 +7,32 @@ import (
 func Execute(funcs []func() error, countParallelExec int, errCount int) {
 	var wg sync.WaitGroup
 	var mutex sync.RWMutex
-	chFuncs := make(chan []func() error, len(funcs))
-	couter := 0
+	chFunc := make(chan func() error, len(funcs))
+	counter := 0
 
-	for i := 0; i < countParallelExec; i++ {
-		chFuncs <- funcs
+	for _, fun := range funcs {
+		chFunc <- fun
 	}
 
 	for i := 0; i < countParallelExec; i++ {
 		wg.Add(1)
-		go func(wg *sync.WaitGroup, mutex *sync.RWMutex, chFuncs <-chan []func() error, counter, errCount int) {
+		go func(wg *sync.WaitGroup, mutex *sync.RWMutex, chFunc <-chan func() error, counter, errCount int) {
 			defer wg.Done()
-			funcs := <-chFuncs
-			for _, fun := range funcs {
-				err := fun()
-				if err != nil {
-					mutex.Lock()
-					couter++
-					mutex.Unlock()
-				}
-
-				mutex.RLock()
-				if couter >= errCount {
-					mutex.RUnlock()
-					return
-				}
-				mutex.RUnlock()
+			fun := <-chFunc
+			err := fun()
+			if err != nil {
+				mutex.Lock()
+				counter++
+				mutex.Unlock()
 			}
-		}(&wg, &mutex, chFuncs, couter, errCount)
+
+			mutex.RLock()
+			if counter >= errCount {
+				mutex.RUnlock()
+				return
+			}
+			mutex.RUnlock()
+		}(&wg, &mutex, chFunc, counter, errCount)
 	}
 
 	wg.Wait()
